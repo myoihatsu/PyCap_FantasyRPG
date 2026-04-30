949fcfe0aef0490223b7916ea678cde35e3505b1
myoihatsu <myoihatsu@gmail.com>
2026-04-30 13:26:00 +0800

    A2: added menu.py and class.py

---
+++ class.py
@@ -0,0 +1,7 @@
+class character:
+    def __init__(self,name,health,mana,action_speed):
+        self._name = name
+        self._health = health
+        self._mana = mana
+        self._action_speed = action_speed
+        
