e9040b46dc7e6d05eb091c57e58e79553d127195
myoihatsu <myoihatsu@gmail.com>
2026-05-01 18:25:19 +0800

    A4: main.py Add: Wizard(Character) int+mp damage logic

--- class.py
+++ class.py
@@ -8,10 +8,27 @@ class Character:
     def get_damage(self):
         return 1 
 
+
 class Grunt(Character):
     def __init__(self, name, health, mana, action_speed,strength):
         super().__init__(name, health, mana, action_speed)
+        
         self.__strength = strength
 
+    #* added simple get_damage
     def get_damage(self):
         return self.__strength
+    
+
+class Wizard(Character):
+    def __init__(self, name, health, mana, action_speed,intelligence):
+        super().__init__(name, health, mana, action_speed)
+
+        self.__intelligence = intelligence
+
+        #? maybe in the future wizard HP is 0.8 effective
+        #? because I planned to include portion of MP as bonus damage
+
+    #* int + mp damage
+    def get_damage(self):
+        return self.__intelligence + 0.2 * self._mana
