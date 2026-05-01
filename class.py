1d3e45cd0ff4265339fdbdaa5ef95bc752e6480c
myoihatsu <myoihatsu@gmail.com>
2026-05-01 18:15:59 +0800

    A3: main.py Add: Grunt(Character) class, get_damage

--- class.py
+++ class.py
@@ -1,7 +1,17 @@
-class character:
+class Character:
     def __init__(self,name,health,mana,action_speed):
         self._name = name
         self._health = health
         self._mana = mana
         self._action_speed = action_speed
-        
+
+    def get_damage(self):
+        return 1 
+
+class Grunt(Character):
+    def __init__(self, name, health, mana, action_speed,strength):
+        super().__init__(name, health, mana, action_speed)
+        self.__strength = strength
+
+    def get_damage(self):
+        return self.__strength
