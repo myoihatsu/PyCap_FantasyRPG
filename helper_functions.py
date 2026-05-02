51a126152a5d5ca332883dbf8cb7c89952a14d77
myoihatsu <myoihatsu@gmail.com>
2026-05-02 15:47:15 +0800

    Added: save character function -> json

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,4 @@
+import json
 
 #* select option function, take int, check if within range, return int
 def select_option(option_range):
@@ -16,3 +17,27 @@ def select_option(option_range):
             print("Err: Out of range.")
         else:
             return selection
+
+
+#* save character creation to a new file .json format and create new file
+def save_character(name,character_data):
+    new_file = "./characters/" + "char_" + name +".json"
+    try:
+        with open(new_file,'w') as temp:
+            json.dump(character_data,temp,indent=4)
+
+    #* I copy from online for exception. 
+    except OSError as e:
+        print(f"File Error: Could not write to disk. {e}")
+    except TypeError as e:
+        print(f"Data Error: Dictionary contains items that can't be JSON. {e}")
+    except Exception as e:
+        print(f"Unexpected Error: {e}")
+
+
+Sauron = {
+    "name":"Sauron",
+    "HP" : 50
+}
+
+save_character("Sauron",Sauron)
