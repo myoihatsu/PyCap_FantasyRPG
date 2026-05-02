54d49d96666b056db0684375f6a79c913d895c5b
myoihatsu <myoihatsu@gmail.com>
2026-05-02 11:54:36 +0800

    A6: renamed: class.py->job_class.py

---
+++ job_class.py
@@ -0,0 +1,45 @@
+class Character:
+    def __init__(self,name,health,mana,action_speed):
+        self._name = name
+        self._health = health
+        self._mana = mana
+        self._action_speed = action_speed
+
+    def get_damage(self):
+        return 1 
+
+
+class Grunt(Character):
+    def __init__(self, name, health, mana, action_speed,strength):
+        super().__init__(name, health, mana, action_speed)
+
+        self.__strength = strength
+
+    #* added simple get_damage
+    def get_damage(self):
+        return self.__strength + 2 * self._health
+    
+
+class Wizard(Character):
+    def __init__(self, name, health, mana, action_speed,intelligence):
+        super().__init__(name, health, mana, action_speed)
+
+        self.__intelligence = intelligence
+
+        #? maybe in the future wizard HP is 0.8 effective
+        #? because I planned to include portion of MP as bonus damage
+
+    #* int + mp damage
+    def get_damage(self):
+        return self.__intelligence + 2 * self._mana
+    
+
+class Ranger(Character):
+    def __init__(self, name, health, mana, action_speed, finesse):
+        super().__init__(name, health, mana, action_speed)
+
+        #* decided finesse sounds nicer instead of dexterity
+        self.__finesse = finesse
+    
+    def get_damage(self):
+        return self.__finesse + 2 * self._action_speed
