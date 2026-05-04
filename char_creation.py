c47bdb4ebaaa0ab7dfd371d7f924eca8870ee651
myoihatsu <myoihatsu@gmail.com>
2026-05-04 12:35:33 +0800

    fbranch, updated char_creation to fully get user_input

--- char_creation.py
+++ char_creation.py
@@ -12,22 +12,34 @@ def choose_class():
         "==========================================\n"
         )
     
-    selection = select_option(3)
-    char_creation(selection)
+    options = select_option(3)
+    char_creation(options)
 
 
 #* Static for now.
 def char_creation(selection):
+    name = user_input.get_text("Insert Character Name: ")
+    health = user_input.get_num("Insert character's initial health: ")
+    mana = user_input.get_num("Insert character's initial mana: ")
+    action_speed = user_input.get_num("Insert character's action_speed: ")
+    
     match selection:
         case 1:
-            new_char = Grunt("Gimli",100,10,0.5,15)
-            print(new_char._name)
+            print("Grunt class uses Strenght(STR) and a portion of HP as damage")
+            stat = user_input.get_num("Insert character's STR: ")
+            new_char = Grunt(name,health,mana,action_speed,stat)
+            save_character(name,new_char)
         case 2:
-            new_char = Wizard("Gandalf",60,30,0.3,25)
-            print(new_char._name)
+            print("Wizard class uses Intelligence(INT) and a portion of MP as damage")
+            stat = user_input.get_num("Insert character's INT: ")
+            new_char = Wizard(name,health,mana,action_speed,stat)
+            save_character(name,new_char)
         case 3:
-            new_char = Ranger("Legolas",50,15,1.2,10)
-            print(new_char._name)
-
+            print("Grunt class uses Finesse(FIN) and a portion of HP as damage")
+            stat = user_input.get_num("Insert character's FOIN: ")
+            new_char = Ranger(name,health,mana,action_speed,stat)
+            save_character(name,new_char)
 
 choose_class()
+
+
